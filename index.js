8ee51b997db96a83b35fe3dff9319210a0b1e02f
Diego Cornejo <diegof.cornejo@gmail.com>
2019-06-29 14:30:02 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var app = express();
+const readXlsxFile = require('read-excel-file/node');
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
+
+app.set('port', process.env.PORT || 4000);
+
+var server = app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + server.address().port);
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    async function processArray(array) {
+        const width = 792;
+        for (const item of array) {
+            var name = item[0] || '';
+            console.log(item);
+            await sleep(500);
+
+            var doc = new PDFDocument({
+                layout: 'landscape'
+            });
+
+            doc.image('./base.jpg', 0, 0, {
+                width: width,
+                height: 612
+            });
+
+            //Name
+            doc.fontSize(24);
+            doc.moveDown(5);
+            doc.text(`${name}`, {
+                align: 'center'
+            });
+
+            //Place
+            doc.fontSize(18);
+            doc.moveDown(0.25);
+            doc.text(`${item[1]}`, {
+                align: 'center'
+            });
+
+            //Course
+            doc.fontSize(24);
+            doc.moveDown(2);
+            doc.text(`${item[2]}`, {
+                align: 'center'
+            });
+
+            //Date
+            doc.fontSize(12);
+            doc.moveDown(2);
+            doc.text(`${item[4]}`, {
+                align: 'center'
+            });
+
+            //Folder
+            var path = `./certs/${name}`;
+            if (!fs.existsSync(path)) {
+                fs.mkdirSync(path);
+            }
+
+            doc.pipe(fs.createWriteStream(`./certs/${item[0]}/${item[0]}_${item[2]}.pdf`));
+            doc.end();
+        };
+    }
+
+    readXlsxFile('./list.xlsx').then((rows) => {
+        processArray(rows);
+    })
+});
